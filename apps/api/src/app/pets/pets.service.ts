ce74e9c01982853d1c5e8b12181db1460b05f67d
TheRealMkadmi <wahib.mkadmi@esprit.tn>
2024-09-02 12:46:35 +0100

    ezzaboubia

--- apps/api/src/app/pets/pets.service.ts
+++ apps/api/src/app/pets/pets.service.ts
@@ -1,8 +1,10 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { EDGE_DB_CLIENT } from 'nest-edgedb';
 import { CrudService } from '../../common/crud.service';
-import { $Pet, Pet } from '../../../dbschema/edgeql-js/modules/default';
+import { $Pet, Pet } from 'dbschema/edgeql-js/modules/default';
 import type { Client } from 'edgedb';
+import e, { $infer } from 'dbschema/edgeql-js';
+import * as console from 'node:console';
 
 @Injectable()
 export class PetsService extends CrudService<$Pet> {
@@ -10,5 +12,16 @@ export class PetsService extends CrudService<$Pet> {
     @Inject(EDGE_DB_CLIENT) protected readonly edgedbClient: Client,
   ) {
     super(Pet, edgedbClient);
+  }
+
+  async findByName(name: string) {
+    const query = e.select(e.Pet, () => ({
+      id: true,
+      name: true,
+    }));
+
+    const k = await query.run(this.edgedbClient);
+    console.table(k);
+    return k;
   }
 }
