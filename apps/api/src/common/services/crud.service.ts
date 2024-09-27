8639f12ac4fe8b4144b0582be192da203cb315e2
TheRealMkadmi <wahib.mkadmi@esprit.tn>
2024-09-27 19:36:42 +0100

    ezzaboubia

--- apps/api/src/common/services/crud.service.ts
+++ apps/api/src/common/services/crud.service.ts
@@ -3,7 +3,7 @@ import e from 'dbschema/edgeql-js';
 import {
   type $scopify,
   ExclusiveTuple,
-  ObjectType, type ObjectTypeExpression,
+  ObjectType, type ObjectTypeExpression, ObjectTypePointers,
   TypeSet,
 } from 'dbschema/edgeql-js/typesystem';
 import {
@@ -18,82 +18,36 @@ import * as $ from '../../../dbschema/edgeql-js/reflection';
 import { $Pet, $PetλShape } from '../../../dbschema/edgeql-js/modules/default';
 import { $objectTypeToTupleType } from '../../../dbschema/edgeql-js/collections';
 import { $expr_Update, UpdateShape } from '../../../dbschema/edgeql-js/update';
-import type * as _std from '../../../dbschema/edgeql-js/modules/std';
-import { $expr_Param } from 'dbschema/edgeql-js/params';
+import type * as _std from "../../../dbschema/edgeql-js/modules/std";
+import { $ObjectType } from 'dbschema/edgeql-js/modules/schema';
 
-export class CrudService {
+
+type IsSubtype<T, U> = T extends U ? true : false;
+type IsStrictSubtype<T, U> = [T] extends [U]
+  ? [U] extends [T]
+  ? false
+  : true
+  : false;
+
+type test = {
+  id: $.PropertyDesc<_std.$uuid, $.Cardinality.One, true, false, true, true>;
+  __type__: $.LinkDesc<$ObjectType, $.Cardinality.One, {}, false, false, true, false>;
+  name: $.PropertyDesc<_std.$str, $.Cardinality.One, false, false, false, false>;
+
+};
+
+export class CrudService<S extends test> {
   constructor(
     protected readonly edgedbClient: Client,
     protected readonly model: $expr_PathNode<
       TypeSet<
-        ObjectType<string, $PetλShape, null, ExclusiveTuple>,
+        ObjectType<string, test, null, ExclusiveTuple>,
         Cardinality.Many
       >,
       null
     >,
   ) {
-  }
 
-  // todo: also ExclusiveTuple needs refinement
-
-  async update<
-    Shape extends {
-      filter?: SelectModifiers['filter'];
-      filter_single?: SelectModifiers<TypeSet<
-        ObjectType<string, $PetλShape, null, ExclusiveTuple>,
-        Cardinality.Many
-      >['__element__']>['filter_single'];
-      set: UpdateShape<TypeSet<
-        ObjectType<string, $PetλShape, null, ExclusiveTuple>,
-        Cardinality.Many
-      >>;
-    },
-  >(
-    shape: (scope: $scopify<TypeSet<
-      ObjectType<string, $PetλShape, null, ExclusiveTuple>,
-      Cardinality.Many
-    >['__element__']>) => Readonly<Shape>,
-  ) {
-    return await e
-      .update(this.model, shape)
-      .run(this.edgedbClient);
-  }
-
-  async insert(
-    data: InsertShape<
-      ObjectType<string, Omit<$PetλShape, 'id'>, null, ExclusiveTuple>
-    >,
-  ) {
-    return await e.insert(this.model, data).run(this.edgedbClient);
-  }
-
-  async insertMany(
-    data: InsertShape<
-      ObjectType<string, Omit<$PetλShape, 'id'>, null, ExclusiveTuple>
-    >[],
-  ) {
-    const m = $objectTypeToTupleType($.$toSet($Pet, $.Cardinality.Many))[
-      '__shape__'
-      ];
-    const query = e.params(
-      {
-        items: e.array(
-          e.tuple({
-            ...m,
-          }),
-        ),
-      },
-      (params: { items: $expr_Param<"items", $.ArrayType<$.NamedTupleType<{ name: _std.$str; }>, `array<${string}>`>, false>; }) => {
-        return e.for(e.array_unpack(params.items), (item) => {
-          return e.insert(this.model, item);
-        });
-      },
-    );
-
-    return await query.run(this.edgedbClient, {
-      // @ts-expect-error
-      items: data,
-    });
   }
 
   async findAll() {
@@ -154,6 +108,66 @@ export class CrudService {
     }));
     return await query.run(this.edgedbClient);
   }
+  // todo: also ExclusiveTuple needs refinement
 
+  async update<
+    Shape extends {
+      filter?: SelectModifiers['filter'];
+      filter_single?: SelectModifiers<TypeSet<
+        ObjectType<string, $PetλShape, null, ExclusiveTuple>,
+        Cardinality.Many
+      >['__element__']>['filter_single'];
+      set: UpdateShape<TypeSet<
+        ObjectType<string, $PetλShape, null, ExclusiveTuple>,
+        Cardinality.Many
+      >>;
+    },
+  >(
+    shape: (scope: $scopify<TypeSet<
+      ObjectType<string, $PetλShape, null, ExclusiveTuple>,
+      Cardinality.Many
+    >['__element__']>) => Readonly<Shape>,
+  ) {
+    return await e
+      .update(this.model, shape)
+      .run(this.edgedbClient);
+  }
+
+  async insert(
+    data: InsertShape<
+      ObjectType<string, Omit<$PetλShape, 'id'>, null, ExclusiveTuple>
+    >,
+  ) {
+    return await e.insert(this.model, data).run(this.edgedbClient);
+  }
+
+  async insertMany(
+    data: InsertShape<
+      ObjectType<string, Omit<$PetλShape, 'id'>, null, ExclusiveTuple>
+    >[],
+  ) {
+    const m = $objectTypeToTupleType($.$toSet($Pet, $.Cardinality.Many))[
+      '__shape__'
+    ];
+    const query = e.params(
+      {
+        items: e.array(
+          e.tuple({
+            ...m,
+          }),
+        ),
+      },
+      (params) => {
+        return e.for(e.array_unpack(params.items), (item) => {
+          return e.insert(this.model, item);
+        });
+      },
+    );
+
+    return await query.run(this.edgedbClient, {
+      // @ts-expect-error
+      items: data,
+    });
+  }
   // todo: find with arbitrary filter and projection
 }
