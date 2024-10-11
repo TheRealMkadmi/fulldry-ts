8abcc0cf855aa3c6b719950ebd52d0c9e731b766
TheRealMkadmi <wahib.mkadmi@esprit.tn>
2024-10-11 15:07:00 +0100

    ezzaboubia

--- apps/api/src/app/user/user.controller.ts
+++ apps/api/src/app/user/user.controller.ts
@@ -3,17 +3,10 @@ import { UserService } from './user.service';
 
 @Controller('pets')
 export class UserController {
-  constructor(private readonly service: UserService) {}
+  constructor(private readonly service: UserService) { }
 
   @Get('/')
   async test() {
     return await this.service.findAll();
-  }
-
-  @Get('/')
-  async test2() {
-    return await this.service.findOneById(
-      '1e3d12d7-630a-11ef-ab07-9531a2453419',
-    );
   }
 }
--- apps/api/src/common/services/base.model.ts
+++ apps/api/src/common/services/base.model.ts
@@ -1,55 +1,28 @@
-import { Client } from 'edgedb';
+import { $, Client } from 'edgedb';
 import e, { Cardinality } from 'dbschema/edgeql-js';
-import __defaultExports, { $PetλShape } from 'dbschema/edgeql-js/modules/default';
-import { $expr_PathNode, LinkDesc, ObjectType, PropertyDesc, TypeSet } from 'dbschema/edgeql-js/reflection';
+import __defaultExports, { $Pet, $PetλShape, Pet } from 'dbschema/edgeql-js/modules/default';
+import { $expr_PathNode, computeTsType, LinkDesc, ObjectType, PropertyDesc, setToTsType, TypeSet } from 'dbschema/edgeql-js/reflection';
 
+export type ExtractTypeSet<T extends keyof __defaultExports> = keyof __defaultExports extends T ? __defaultExports[T] extends $expr_PathNode<infer U, any> ? U : never : never;
+export type SelectResult<T extends keyof __defaultExports> = setToTsType<ExtractTypeSet<T>>;
 
-
-type ValidNameSpaceKeys = Exclude<{
-  [K in keyof __defaultExports]: __defaultExports[K] extends $expr_PathNode ? K : never;
-}[keyof __defaultExports], 'BaseObject' | 'Object' | 'FreeObject'>;
 
 export class Model<
-  TModelName extends ValidNameSpaceKeys = ValidNameSpaceKeys,
+  TModelName extends keyof __defaultExports,
 > {
   constructor(
-    private readonly modelName: TModelName,
+    private readonly modelName: keyof __defaultExports,
     private readonly client: Client,
   ) {
   }
 
-  async findAll() {
-    const model = e[this.modelName as ValidNameSpaceKeys];
-    return e.select(model, (m) => ({
-      ...m['*'],
-    })).run(this.client);
-  }
-
-  async findOneById(id: string) {
-    const model = e[this.modelName as ValidNameSpaceKeys];
-
+  async findAll(): Promise<SelectResult<TModelName>> {
+    const model = e[this.modelName];
     return await e
-      .select(model, (model) => ({
-        ...model['*'],
-        filter_single: e.op(model.id, '=', e.uuid(id)),
+      .select(model, (m) => ({
+        ...m['*'],
       }))
-      .run(this.client);
+      .run(this.client) as SelectResult<TModelName>;
   }
-
-  async findManyByIds(ids: string[]) {
-    const model = e[this.modelName as ValidNameSpaceKeys];
-
-    return await e
-      .select(model, (model) => ({
-        ...model['*'],
-        filter: e.op(
-          model.id,
-          'in',
-          e.array_unpack(e.literal(e.array(e.str), ids)),
-        ),
-      }))
-      .run(this.client);
-  }
-
-
 }
+
