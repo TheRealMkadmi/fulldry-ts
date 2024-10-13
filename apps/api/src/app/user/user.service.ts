85aef84fe4ed83f4e7e7714845219db94a16465f
TheRealMkadmi <wahib.mkadmi@esprit.tn>
2024-10-13 15:58:53 +0100

    I give up

--- apps/api/src/app/user/user.service.ts
+++ apps/api/src/app/user/user.service.ts
@@ -5,12 +5,15 @@ import { Model } from '../../common/services/base.model';
 import { Pet } from 'dbschema/edgeql-js/modules/default';
 
 @Injectable()
-export class UserService extends Model<typeof Pet> {
+export class UserService extends Model<'Pet'> {
   constructor(@Inject(EDGE_DB_CLIENT) protected readonly edgedbClient: Client) {
-    super(Pet, edgedbClient);
+    super('Pet', edgedbClient);
   }
 
   async findAll() {
+
+
+
     return await super.findAll();
   }
 }
--- apps/api/src/common/services/base.model.ts
+++ apps/api/src/common/services/base.model.ts
@@ -2,25 +2,64 @@
 import { Client } from 'edgedb';
 import e from 'dbschema/edgeql-js';
 import __defaultExports from 'dbschema/edgeql-js/modules/default';
-import { $expr_PathNode } from 'dbschema/edgeql-js/reflection';
 
-type EntityNames = keyof __defaultExports;
-type Entities = typeof __defaultExports[EntityNames];
 
-export class Model<K extends EntityNames> {
-  private readonly model: Entities;
-  private readonly client: Client;
+type EntityMap = typeof __defaultExports;
 
-  constructor(key: K, client: Client) {
-    this.model = __defaultExports[key];
-    this.client = client;
-  }
 
-  async findAll() {
-    return await e
-      .select(this.model, (m) => ({
-        ...m['*'],
-      }))
-      .run(this.client);
-  }
-}
+
+type IsAny<T> = (
+  0 extends (1 & T) ?
+  true :
+  false
+);
+
+type IsNever<T> = (
+  [T] extends [never] ?
+  (
+    IsAny<T> extends true ?
+    false :
+    true
+  ) :
+  false
+);
+
+type IsStringLiteral<T extends string> = (
+  IsAny<T> extends true ?
+  false :
+  IsNever<T> extends true ?
+  false :
+  string extends T ?
+  false :
+  true
+);
+
+type IsOneStringLiteral<T extends string> = (
+  IsStringLiteral<T> extends true ?
+  (
+    {
+      [k in T]: (
+        Exclude<T, k> extends never ?
+        true :
+        false
+      )
+    }[T]
+  ) :
+  false
+);
+
+type EntityNames = Extract<keyof __defaultExports, string>;
+
+declare function GetConfiguration<K extends EntityNames>(
+  key: (
+    IsOneStringLiteral<K> extends true ?
+    K :
+    [K, "is not a single string literal"]
+  )
+): __defaultExports[K];
+
+const test = GetConfiguration<'User'>('User');
+
+
+
+export class BaseRepository { }
