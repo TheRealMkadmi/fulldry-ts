9804bd065ae1b557e4eea27833f9853039189f5b
TheRealMkadmi <wahib.mkadmi@esprit.tn>
2024-10-13 21:47:38 +0100

    asba

--- apps/api/src/common/services/base.model.ts
+++ apps/api/src/common/services/base.model.ts
@@ -2,7 +2,6 @@
 import __defaultExports, { $User, $UserλShape, Pet, User } from 'dbschema/edgeql-js/modules/default';
 import type * as _std from "dbschema/edgeql-js/modules/std";
 import e from 'dbschema/edgeql-js';
-
 import * as _ from "dbschema/edgeql-js/imports";
 import { Client } from 'edgedb';
 
@@ -10,27 +9,44 @@ import { Client } from 'edgedb';
 type UnionToIntersection<U> =
     (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never
 
-export type Models = typeof Pet | typeof User;
+type StrictIdentity<T> = T & UnionToIntersection<T>
+type RecursiveUnionToIntersection<U> =
+    UnionToIntersection<U> extends infer I
+    ? {
+        [K in keyof I]: I[K] extends object
+        ? RecursiveUnionToIntersection<I[K]>
+        : I[K]
+    }
+    : never;
 
+export type ModelNames = keyof __defaultExports;
+export type Models = typeof __defaultExports[ModelNames];
 
-export class BaseRepository<TModel extends Models> {
+// create a mapped type
+export type ModelMap = {
+    [K in ModelNames]: RecursiveUnionToIntersection<__defaultExports[K]>
+}
+
+export class BaseRepository<TModel extends Models, TModelName = ModelNames> {
 
     constructor(
-        protected readonly model: TModel & UnionToIntersection<TModel>,
+        protected readonly modelName: TModelName,
+        protected readonly model: StrictIdentity<TModel>,
         protected readonly edgedbClient: Client,
     ) { }
 
 
     async findAll() {
-        return await e
+        const query = e
             .select(this.model, (m) => ({
                 ...m['*'],
             }))
-            .run(this.edgedbClient);
+        const result = await query.run(this.edgedbClient);
+        return result;
     }
 }
 
-export class test extends BaseRepository<typeof User | typeof User> {
+export class test extends BaseRepository<typeof User> {
     constructor(edgedbClient: Client) {
         super(User, edgedbClient);
     }
