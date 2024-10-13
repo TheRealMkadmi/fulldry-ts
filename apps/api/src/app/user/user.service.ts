1b2d5c2d75c94ef614b82dfe59732ec9be41abc4
TheRealMkadmi <wahib.mkadmi@esprit.tn>
2024-10-13 19:12:55 +0100

    na3ambouk w bou zokomek

--- apps/api/src/app/user/user.service.ts
+++ apps/api/src/app/user/user.service.ts
@@ -1,19 +1,16 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { EDGE_DB_CLIENT } from 'nest-edgedb';
 import { type Client } from 'edgedb';
-import { Model } from '../../common/base.model';
-import { Pet } from 'dbschema/edgeql-js/modules/default';
+import { BaseRepository } from 'src/common/services/base.model';
+import { $User, User } from 'dbschema/edgeql-js/modules/default';
 
 @Injectable()
-export class UserService extends Model<'Pet'> {
+export class UserService extends BaseRepository<typeof User> {
   constructor(@Inject(EDGE_DB_CLIENT) protected readonly edgedbClient: Client) {
-    super('Pet', edgedbClient);
+    super(User, edgedbClient);
   }
 
   async findAll() {
-
-
-
     return await super.findAll();
   }
 }
--- apps/api/src/common/services/base.model.ts
+++ apps/api/src/common/services/base.model.ts
@@ -1,80 +1,41 @@
 // model.ts
 import { Client } from 'edgedb';
 import e from 'dbschema/edgeql-js';
-import __defaultExports from 'dbschema/edgeql-js/modules/default';
+import __defaultExports, { $User, $UserλShape } from 'dbschema/edgeql-js/modules/default';
+import type * as _std from "dbschema/edgeql-js/modules/std";
 
+import * as $ from "dbschema/edgeql-js/reflection";
+import * as _ from "dbschema/edgeql-js/imports";
+import { $ObjectType } from 'dbschema/edgeql-js/modules/schema';
 
-type EntityMap = typeof __defaultExports;
-type EntityNames = keyof EntityMap;
+export type BaseShape = {
+    id: $.PropertyDesc<_std.$uuid, $.Cardinality.One, true, false, true, true>;
+    __type__: $.LinkDesc<$ObjectType, $.Cardinality.One, {}, false, false, true, false>;
+}
 
+type m = $.computeTsType<$User, $.Cardinality.One>;
 
+export abstract class BaseRepository
+    <
+        T extends $.$expr_PathNode<$.TypeSet<$User, $.Cardinality.Many>, null>,
+        S extends $.BaseType = T['__element__'],
+        C extends $.Cardinality = T['__cardinality__']
 
-type IsAny<T> = (
-    0 extends (1 & T) ?
-    true :
-    false
-);
+    > {
 
-type IsNever<T> = (
-    [T] extends [never] ?
-    (
-        IsAny<T> extends true ?
-        false :
-        true
-    ) :
-    false
-);
-
-type IsStringLiteral<T extends string> = (
-    IsAny<T> extends true ?
-    false :
-    IsNever<T> extends true ?
-    false :
-    string extends T ?
-    false :
-    true
-);
-
-type IsOneStringLiteral<T extends string> = (
-    IsStringLiteral<T> extends true ?
-    (
-        {
-            [k in T]: (
-                Exclude<T, k> extends never ?
-                true :
-                false
-            )
-        }[T]
-    ) :
-    false
-);
-
-declare function GetConfiguration<K extends Extract<keyof __defaultExports, string>>(
-    key: (
-        IsOneStringLiteral<K> extends true ?
-        K :
-        [K, "is not a single string literal"]
-    )
-): __defaultExports[K];
-
-const test = GetConfiguration<'User'>('User');
-
-
-
-class GenericModel<K extends Extract<keyof __defaultExports, string>> {
-    model: __defaultExports[K];
-
-    constructor(key: K) {
-        this.model = GetConfiguration(key as IsOneStringLiteral<K> extends true ? K : never);
+    constructor(
+        protected readonly model: $.$expr_PathNode,
+        protected readonly edgedbClient: Client
+    ) {
     }
 
-    async findAll(client: Client) {
+    async findAll() {
         return await e
             .select(this.model, (m) => ({
                 ...m['*'],
             }))
-            .run(client);
+            .run(this.edgedbClient) as $.computeTsType<S, C>;
     }
+
 }
 
-const model = new GenericModel('User');
