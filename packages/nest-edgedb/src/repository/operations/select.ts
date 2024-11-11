c85847c3697e428452416d4396c3862f9078b851
TheRealMkadmi <wahib.mkadmi@esprit.tn>
2024-11-11 15:31:03 +0100

    Add findOneByIdWithProjection method to Wrapper class for enhanced querying

--- packages/nest-edgedb/src/repository/operations/select.ts
+++ packages/nest-edgedb/src/repository/operations/select.ts
@@ -1,22 +1,19 @@
 import { Client } from "edgedb";
 import { $expr_PathNode, objectTypeToSelectShape } from "../../generated/syntax/syntax";
-import { computeSelectShapeResult, ManyCompleteProjections, ModelIdentityArray, ModelScope, ModelTypeSet, OneCompleteProjection } from "../types";
+import { computeSelectShapeResult, FilterSingleType, ManyCompleteProjections, ModelIdentityArray, ModelScope, ModelTypeSet, OneCompleteProjection } from "../types";
 import e from '../../generated/syntax/';
 import { $overload, Coerce, HKOperation } from 'fulldry-utils';
 
-type ModelsTuple = [typeof e.Pet];
 
 interface $RenderFindAll extends HKOperation {
     new: (x: Coerce<this["_1"], unknown>) => <const T extends Coerce<this["_1"], unknown> & $expr_PathNode>(x: T) => Promise<ManyCompleteProjections<T>>;
 }
 
-// ____________
 
 interface $RenderFindAllIds extends HKOperation {
     new: (x: Coerce<this["_1"], unknown>) => <const T extends Coerce<this["_1"], unknown> & $expr_PathNode>(x: T, limit?: number, offset?: number) => Promise<ModelIdentityArray>;
 }
 
-// ____________
 
 
 interface $RenderFindManyByIds extends HKOperation {
@@ -25,7 +22,6 @@ interface $RenderFindManyByIds extends HKOperation {
     >(x: T, ids: string[]) => Promise<ManyCompleteProjections<T>>;
 }
 
-// ____________
 
 interface $RenderFindOneById extends HKOperation {
     new: (x: Coerce<this["_1"], unknown>) =>
@@ -33,7 +29,13 @@ interface $RenderFindOneById extends HKOperation {
             => Promise<OneCompleteProjection<T>>;
 }
 
-// ____________
+
+interface $RenderFindOneByIdWithProjection extends HKOperation {
+    new: (x: Coerce<this["_1"], unknown>) => <
+        const T extends Coerce<this["_1"], unknown>,
+        Shape extends objectTypeToSelectShape<ModelTypeSet<T>["__element__"]>,
+    >(x: T, id: string, shape: (scope: ModelScope<T>) => Readonly<Shape>) => Promise<computeSelectShapeResult<T, Shape & FilterSingleType>>;
+}
 
 class Wrapper<Models extends $expr_PathNode[]> {
     constructor(
@@ -85,35 +87,26 @@ class Wrapper<Models extends $expr_PathNode[]> {
                 }))
                 .run(client);
         }
+
+    // @ts-expect-error
+    findOneByIdWithProjection: $overload<Models, $RenderFindOneByIdWithProjection> =
+        async <T, Shape>(model: T, id: string, shape: (scope: ModelScope<T>) => Readonly<Shape>) => {
+            return await e
+                .select(model, (m: any) => ({
+                    ...shape(m),
+                    filter_single: e.op(m.id, '=', e.literal(e.str, id)),
+                }))
+                .run(client);
+        }
 }
 const client = {} as Client;
+type ModelsTuple = [typeof e.Pet];
 const nam = new Wrapper<ModelsTuple>(client);
 const r = nam.findAll(e.Pet);
 const f = nam.findAllIds(e.Pet);
 const g = nam.findManyByIds(e.Pet, ['1', '2']);
+const h = nam.findOneById(e.Pet, '1');
+const i = nam.findOneByIdWithProjection(e.Pet, '1', (m) => ({
+    name: true,
+}));
 
-
-
-// _____________
-
-
-
-
-
-
-
-// _____________
-
-type RenderFindOneByIdWithProjection<OneOfPossibleOptions> = <
-    const T extends OneOfPossibleOptions,
-    Shape extends objectTypeToSelectShape<ModelTypeSet<T>["__element__"]>,
->(client: Client, x: T, id: string, shape: (scope: ModelScope<T>) => Readonly<Shape>) => Promise<computeSelectShapeResult<T, Shape & FilterSingleType>>;
-const findOneByIdWithProjection: $overload<RenderFindOneByIdWithProjection<Models>> =
-    async <T, Shape>(client: Client, model: T, id: string, shape: (scope: ModelScope<T>) => Readonly<Shape>) => {
-        return await e
-            .select(model, (m: any) => ({
-                ...shape(m),
-                filter_single: e.op(m.id, '=', e.literal(e.str, id)),
-            }))
-            .run(client);
-    }
