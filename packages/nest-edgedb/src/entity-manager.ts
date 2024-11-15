25b4dacd0b53cad7f8993529767ebb853b0cf0df
TheRealMkadmi <wahib.mkadmi@esprit.tn>
2024-11-15 13:57:18 +0100

    ?

--- packages/nest-edgedb/src/entity-manager.ts
+++ packages/nest-edgedb/src/entity-manager.ts
@@ -27,12 +27,27 @@ const petNames = em.findOneByIdWithProjection(e.Pet, client, '1', (m) => ({
 }));
 
 
-function wrap<T extends $expr_PathNode, R>(firstArg: T, secondArg: Client, func: (x: T, c: Client, ...args: readonly unknown[]) => R) {
+function wrapFindAll<T extends $expr_PathNode, R>(firstArg: T, secondArg: Client, func: (x: T, c: Client, ...args: readonly unknown[]) => R) {
     return function (...newArgs: unknown[]) {
         return func(firstArg, secondArg, ...newArgs);
     }
 }
 
-const r = wrap(e.User, client, em.findAll);
+const r = wrapFindAll(e.Pet, client, em.findAll);
 
-const pets = r(); 
+const pets = r();
+
+
+// ___________
+
+
+const wrapFindOne =
+    <T extends $expr_PathNode, R>
+        (firstArg: T, secondArg: Client, func: (x: T, c: Client, ...args: readonly [string]) => R) => (id: string) => func(firstArg, secondArg, id);
+
+const rOne = wrapFindOne(e.Pet, client, em.findOneById);
+
+const pes = rOne('1');
+
+// ______
+
