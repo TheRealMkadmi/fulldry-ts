5bf3b00da2ae3305ecf5d064fc0ccfcf5196bfa2
TheRealMkadmi <wahib.mkadmi@esprit.tn>
2024-11-23 17:02:19 +0100

    .

--- packages/nest-edgedb/src/entity-manager.ts
+++ packages/nest-edgedb/src/entity-manager.ts
@@ -1,13 +1,38 @@
-import { $expr_PathNode } from "./generated/syntax/syntax";
+import { $expr_PathNode, group, insert, update } from "./generated/syntax/syntax";
 import { $overload } from 'fulldry-utils';
-import { $RenderFindAll, $RenderFindOneById, $RenderFindOneByIdWithProjection, findAll, findOneById, findOneByIdWithProjection } from "./operations/select";
+import { $RenderCount, $RenderExists, $RenderFind, $RenderFindAll, $RenderFindAllIds, $RenderFindManyByIds, $RenderFindOneById, $RenderFindOneByIdWithProjection, $RenderPaginate, count, exists, findAll, findAllIds, findManyByIds, findOneById, findOneByIdWithProjection } from "./operations/select";
+import { sum, min, max } from "ramda";
+import { $RenderSum, $RenderMin, $RenderMax } from "./operations/aggregate";
+import { $RenderDeleteSingle, deleteOne, $RenderDeleteMultiple, deleteMany } from "./operations/delete";
+import { $RenderGroup } from "./operations/group";
+import { $RenderInsertSingle, $RenderInsertMultiple, insertMany } from "./operations/insert";
+import { $RenderUpdate, $RenderUpdateMany, updateMany } from "./operations/update";
 
 export class EntityManager<TModels extends readonly $expr_PathNode[]> {
     findAll: $overload<TModels, $RenderFindAll> = findAll as any;
     findOneByIdWithProjection: $overload<TModels, $RenderFindOneByIdWithProjection> = findOneByIdWithProjection as any
-    findOneById: $overload<TModels, $RenderFindOneById> = findOneById as any
-}
+    findOneById: $overload<TModels, $RenderFindOneById> = findOneById as any;
+    findAllIds: $overload<TModels, $RenderFindAllIds> = findAllIds as any;
+    findManyByIds: $overload<TModels, $RenderFindManyByIds> = findManyByIds as any;
+    find: $overload<TModels, $RenderFind> = findAll as any;
+    paginate: $overload<TModels, $RenderPaginate> = findAll as any;
 
+    sum: $overload<TModels, $RenderSum> = sum as any;
+    min: $overload<TModels, $RenderMin> = min as any;
+    max: $overload<TModels, $RenderMax> = max as any;
+    group: $overload<TModels, $RenderGroup> = group as any;
+    count: $overload<TModels, $RenderCount> = count as any;
+    exists: $overload<TModels, $RenderExists> = exists as any;
+
+    insert: $overload<TModels, $RenderInsertSingle> = insert as any;
+    insertMany: $overload<TModels, $RenderInsertMultiple> = insertMany as any;
+
+    update: $overload<TModels, $RenderUpdate> = update as any;
+    updateMany: $overload<TModels, $RenderUpdateMany> = updateMany as any;
+
+    deleteOne: $overload<TModels, $RenderDeleteSingle> = deleteOne as any;
+    deleteMany: $overload<TModels, $RenderDeleteMultiple> = deleteMany as any;
+}
 
 export type NarrowEntityManager<T extends $expr_PathNode> = EntityManager<[T]>;
 
